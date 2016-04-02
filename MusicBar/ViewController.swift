038b4edd616bb8cec2e2ae95b880bda9680e3d15
Tomohiro Nishimura <tomohiro68@gmail.com>
2016-04-02 20:18:00 +0900

    Initial Commit

---
+++ MusicBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicBar
+//
+//  Created by Tomohiro Nishimura on 2016/04/02.
+//  Copyright © 2016年 Tomohiro Nishimura. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
