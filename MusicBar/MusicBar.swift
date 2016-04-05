9a19322692751c8e4ace2a2bf5f156e4e0cec063
Tomohiro Nishimura <tomohiro68@gmail.com>
2016-04-06 00:23:29 +0900

    ちょっきんの履歴を表示する

--- MusicBar/MusicBar.swift
+++ MusicBar/MusicBar.swift
@@ -33,6 +33,23 @@ private struct Track {
 class MusicBar: NSObject {
     private let iTunesNotificationName = "com.apple.iTunes.playerInfo"
 
+
+    private static let HOST = NSURL(string: "http://localhost:4567/")!
+
+    enum Endpoint {
+        case Listen
+        case Recents
+
+        func URL() -> NSURL {
+            switch self {
+            case .Listen:
+                return NSURL(string: "/listen", relativeToURL: HOST)!
+            case .Recents:
+                return NSURL(string: "/recents", relativeToURL: HOST)!
+            }
+        }
+    }
+
     private var track: Track?
 
     private let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
@@ -42,6 +59,12 @@ class MusicBar: NSObject {
     private let playingImage = NSImage(named: "playing")!
     private let pauseImage   = NSImage(named: "pause")!
 
+    private var currentTracks: [Track] = []
+    // XXX: ちゃんとしたい
+    private let currentTrackMenuItems = [
+        NSMenuItem(), NSMenuItem(), NSMenuItem(), NSMenuItem(), NSMenuItem()
+    ]
+
     override init() {
         super.init()
 
@@ -49,13 +72,23 @@ class MusicBar: NSObject {
         separatorMenuItem.hidden = true
 
         songInfoMenuItem.target = self
-        songInfoMenuItem.action = #selector(openStore)
+        songInfoMenuItem.action = #selector(openCurrentTrackStore)
 
         let menu = NSMenu()
         menu.autoenablesItems = true
         menu.addItem(songInfoMenuItem)
         menu.addItem(separatorMenuItem)
-        let openSiteMenuItem = menu.addItemWithTitle("サイトみる", action: #selector(openSite), keyEquivalent: "")
+        let openSiteMenuItem = menu.addItemWithTitle("サイトをみる", action: #selector(openSite), keyEquivalent: "")
+
+        menu.addItem(NSMenuItem.separatorItem())
+
+        for (index, menuItem) in currentTrackMenuItems.enumerate() {
+            menu.addItem(menuItem)
+            menuItem.target = self
+            menuItem.action = Selector("openStore\(index + 1)")
+            menuItem.hidden = true
+        }
+
         openSiteMenuItem?.target = self
         menu.addItem(NSMenuItem.separatorItem())
         let quitMenuItem = menu.addItemWithTitle("Quit", action: #selector(quit), keyEquivalent: "q")
@@ -73,11 +106,37 @@ class MusicBar: NSObject {
             updateSongInfo()
         }
 
+        // サイトの再生履歴を取得する
+        updateRecents()
+
         NSDistributedNotificationCenter.defaultCenter().addObserver(self, selector: #selector(trackChanged(_:)), name: iTunesNotificationName, object: nil)
     }
 
     deinit {
         NSDistributedNotificationCenter.defaultCenter().removeObserver(self, name: iTunesNotificationName, object: nil)
+    }
+
+    func updateRecents() {
+        currentTracks.removeAll()
+        currentTrackMenuItems.forEach { $0.hidden = true }
+
+        Alamofire.request(.GET, Endpoint.Recents.URL(), parameters: [ "limit" : currentTrackMenuItems.count ])
+            .responseJSON { [weak self] response in
+                switch response.result {
+                case .Success(let recents):
+                    if let recents = recents as? [[String : String]] {
+                        for (index, trackInfo) in recents.enumerate() {
+                            let track = Track(title: trackInfo["track_name"]!, artist: trackInfo["artist_name"]!, album: trackInfo["collection_name"]!, storeURL: NSURL(string: trackInfo["track_view_url"]!)!)
+                            let menuItem = self?.currentTrackMenuItems[index]
+                            menuItem?.title = "「\(track.title)」 by \(track.artist)"
+                            menuItem?.hidden = false
+                            self?.currentTracks.append(track)
+                        }
+                    }
+                case .Failure(let error):
+                    print(error)
+                }
+            }
     }
 
     override func validateMenuItem(menuItem: NSMenuItem) -> Bool {
@@ -121,16 +180,16 @@ class MusicBar: NSObject {
 
             if let storeID = track?.storeID {
                 let parameters = [ "product_id" : storeID ]
-                Alamofire.request(.POST, "http://localhost:4567/listen", parameters: parameters)
+                Alamofire.request(.POST, Endpoint.Listen.URL(), parameters: parameters)
             }
-
         } else {
             track = nil
         }
         updateSongInfo()
+        updateRecents()
     }
 
-    func openStore() {
+    func openCurrentTrackStore() {
         guard let storeURL = track?.storeURL else {
             return
         }
@@ -144,6 +203,55 @@ class MusicBar: NSObject {
     
     func quit() {
         NSApplication.sharedApplication().terminate(self)
+    }
+
+    // 最悪
+
+    func openStore1() {
+        guard currentTracks.count > 0 else {
+            return
+        }
+        let track = currentTracks[0]
+        openStore(for: track)
+    }
+
+    func openStore2() {
+        guard currentTracks.count > 1 else {
+            return
+        }
+        let track = currentTracks[1]
+        openStore(for: track)
+    }
+
+    func openStore3() {
+        guard currentTracks.count > 2 else {
+            return
+        }
+        let track = currentTracks[2]
+        openStore(for: track)
+    }
+
+    func openStore4() {
+        guard currentTracks.count > 3 else {
+            return
+        }
+        let track = currentTracks[3]
+        openStore(for: track)
+    }
+
+    func openStore5() {
+        guard currentTracks.count > 4 else {
+            return
+        }
+        let track = currentTracks[4]
+        openStore(for: track)
+    }
+
+    private func openStore(for track: Track) {
+        guard let storeURL = track.storeURL else {
+            return
+        }
+        NSWorkspace.sharedWorkspace().openURL(storeURL)
     }
 }
 
